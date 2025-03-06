780a9e2cc3c264df2fae7878b51192503311f3aa
root <root@DESKTOP-N6T8U1T.>
2025-03-06 15:08:31 +0200

    positive or negative

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+i=0
+if i > number:
+    print(number,"is negative")
+elif i < number:
+    print(number,"is positive")
+else:
+    print(number,"is zero")
